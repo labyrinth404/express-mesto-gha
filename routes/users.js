f007ea43b925eb2a5e1cec777e6edd475077bfa3
labyrinth404 <48043627+labyrinth404@users.noreply.github.com>
2022-08-18 20:03:59 +0700

    celeb7.1.1

--- routes/users.js
+++ routes/users.js
@@ -7,19 +7,21 @@ const {
   updateUserAvatar,
 } = require('../controllers/users');
 
-router.get('/:id', celebrate({
-  params: Joi.object().keys({
-    id: Joi.string().required().hex().length(24),
-  }),
-}), getUser);
-
 router.get('/', getUsers);
+
 router.patch('/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
   }),
 }), updateUser);
+
+router.get('/:id', celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().required().hex().length(24),
+  }),
+}), getUser);
+
 
 router.patch('/me/avatar', celebrate({
   body: Joi.object().keys({
